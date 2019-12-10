14929be4884506cf4997f82c2f59586b5f92a659
Samuli Romo <samuli.s.romo@gmail.com>
2019-12-10 17:04:08 +0100

    set index to datetime

--- pipeline.py
+++ pipeline.py
@@ -3,13 +3,16 @@ import matplotlib.pyplot as plt
 
 #read data and remove unnecessary variables
 df = pd.read_csv('minute_weather.csv')
+df.rename(columns={'hpwren_timestamp': 'time'}, inplace=True)
 df.drop(['rowID', 'avg_wind_direction', 'max_wind_direction', 'min_wind_direction', 'max_wind_speed', 'min_wind_speed','rain_duration'], axis=1, inplace=True)
-print(df.head())
-print(df.info())
 df.dropna(inplace=True)
-print(df.info())
-"""
+df['time'] = pd.to_datetime(df['time'])
+df.set_index('time', inplace=True)
+#print(df.info())
+print(df.head())
+
 #show the correlation matrix
+"""
 f = plt.figure(figsize=(19, 15))
 plt.matshow(df.corr(), fignum=f.number)
 plt.xticks(range(df.shape[1]), df.columns, fontsize=14, rotation=90)
@@ -18,6 +21,10 @@ cb = plt.colorbar()
 cb.ax.tick_params(labelsize=14)
 #plt.show()
 """
+
+#graph temperature over time
+"""
 ax = plt.gca()
-df.plot(kind='line',x='hpwren_timestamp',y='air_temp',ax=ax)
+df.plot(kind='line',y='air_temp',ax=ax)
 plt.show()
+"""
