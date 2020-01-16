aabc8836a7de8d808fc008fdc21e6f5e68fe062d
Samuli Romo <samuli.s.romo@gmail.com>
2020-01-16 17:55:54 +0100

    Added methods for balancing and normalization

--- pipeline.py
+++ pipeline.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import random
 import matplotlib.pyplot as plt
 from sklearn import preprocessing
 
@@ -32,20 +33,21 @@ ax = plt.gca()
 df.plot(kind='line',y='air_temp',ax=ax)
 plt.show()
 """
-#=======================================
-def split_validation(data):
-  times = sorted(data.index.values)
-  last_5pct = times[-int(0.05*len(times))]
-  validation_df = data[(df.index >= last_5pct)]
-  main_df = data[(main_df.index < last_5pct)]
-  return main_df, validation_df
-#=======================================
-
+#============================================================
 
 SEQ_LEN = 60
 PREDICT_PERIOD_LEN = 60
 TARGET_VAR = 'air_temp'
 TARGET_INDEX = df.columns.get_loc(TARGET_VAR)
+
+
+def split_validation(data):
+  times = sorted(data.index.values)
+  last_5pct = times[-int(0.05*len(times))]
+  validation_df = data[(data.index >= last_5pct)]
+  main_df = data[(data.index < last_5pct)]
+  return main_df, validation_df
+
 
 def classify(current_seq, future_seq):
   data = []
@@ -55,6 +57,7 @@ def classify(current_seq, future_seq):
     return 1
   else:
     return 0
+
 
 def make_sequences(data):
   print('Creating sequences.....')
@@ -69,18 +72,56 @@ def make_sequences(data):
       pred_seq = []
       for n in range(index, index+PREDICT_PERIOD_LEN):
         pred_seq.append(data[TARGET_VAR][n])
-      #sequence.append(classify(sequence, pred_seq))
-      sequential_data.append([sequence, classify(sequence, pred_seq)])
+      sequential_data.append([np.array(sequence), classify(sequence, pred_seq)])
       sequence = []
-      print(index, len(sequential_data))
     sequence.append(i)
     n_seq +=1
-    if len(sequential_data) == 10: #for testing purposes only
-      break
   return sequential_data
 
 
-data = make_sequences(df)
+def balance(data):
+  class_0 = []
+  class_1 = []
+  for seq, target in data:
+    if target == 0:
+      class_0.append([seq, target])
+    elif target == 1:
+      class_1.append([seq, target])
+  random.shuffle(class_0)
+  random.shuffle(class_1)
+  lower = min(len(class_0), len(class_1))
+  class_0 = class_0[:lower]
+  class_1 = class_1[:lower]
+  sequential_data = class_0 + class_1
+  random.shuffle(sequential_data)
+  X = []
+  y = []
+  for seq, target in sequential_data:
+    X.append(seq)
+    y.append(target)
+  return np.array(X), y
 
-for seq, target in data:
-  print(len(seq), target)
+
+def normalize(data):
+  normalized = np.reshape(data, ((len(data)*SEQ_LEN), 5))
+  scaler = preprocessing.MinMaxScaler()
+  scaler.fit(normalized)
+  normalized = scaler.transform(normalized)
+  normalized = np.reshape(normalized, (int((len(normalized)/SEQ_LEN)), SEQ_LEN, 5))
+  return normalized
+
+
+#===============================================================================
+main_df, validation_df = split_validation(df)
+
+main_df = make_sequences(main_df)
+train_x, train_y = balance(main_df)
+train_x = normalize(train_x)
+
+validation_df = make_sequences(validation_df)
+test_x, test_y = balance(validation_df)
+test_x = normalize(test_x)
+
+print(f"train data: {len(train_x)} validation: {len(test_x)}")
+print(f"Negatives: {train_y.count(0)}, positives: {train_y.count(1)}")
+print(f"VALIDATION negatives: {test_y.count(0)}, positives: {test_y.count(1)}")
